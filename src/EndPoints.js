00dcd0ae1f817ca6df033180e2f284a9a87dc54c
jonatas <jonatasprado445@gmail.com>
2022-05-24 09:46:09 -0300

    quase rponto

--- src/EndPoints.js
+++ src/EndPoints.js
@@ -1,12 +1,8 @@
 
-import  {dobro,somar,media,temp, dobro} from './f1.js'
-import 'dotenv/config'
+import  {dobro,somar,media,temp, corsimcornao, ingresso, FreqCaracter, tabuada} from './Services.js'
+
 import { Router } from 'express'
-
-
-const server =Router();
-
-
+const server = Router();
 
 
 
@@ -15,13 +11,13 @@ server.get('/ping', (req,resp) =>{
 })
 
 server.get('/dobro/:numero' ,(req,resp)=>{
-    let numero = Number(req.params.numero);
-    let dobro = dobro(numero)
+    const numero = Number(req.params.numero);
+    const d = dobro(numero)
     
 
     resp.send({
-        dobro: dobro
-    }) 
+        dobro: d
+    }); 
 
 })
 
@@ -32,7 +28,7 @@ server.get('/somar', (req,resp) => {
     const d = somar(a,b)
     resp.send({
         soma:d
-    })
+    });
 })
 
 server.post('/somar', (req,resp)=> {
@@ -55,10 +51,65 @@ server.post('/somar', (req,resp)=> {
 server.post('/temp',(req,resp)=>{
     const a = req.body;
 
-    const d = temp (a)
+    const d = temp (a);
     resp.send({
         temp:d
     })
+})
+
+server.get('/temp/:numero' ,(req,resp)=>{
+    const numero = Number(req.params.numero);
+    const d = temp(numero)
+
+    resp.send({
+        temp: d
+    }); 
+
+})
+
+server.post('/media', (req,resp)=>{
+    const {a, b, c} = req.body;
+    
+    const x = media(a, b, c);
+    resp.send({
+        media: x
+    });
+})
+server.get('/corsimcornao/:cor', (req,resp)=>{
+     const cor = req.params.cor;
+     const x = corsimcornao(cor);
+
+     resp.send({
+         corsimcornao: x
+     });
+
+})
+server.post('/ingresso', (req,resp)=>{
+    const {dia:dia, inteira:inteira, meia:meia, nacionalidade:nacionalidade} = req.body;
+    const x = ingresso(String(dia), inteira, meia, String(nacionalidade));
+
+    resp.send({
+        ingresso: x
+    });
+})
+
+server.get('/FreqCaracter/:texto/:caracter', (req,resp) =>{
+    const texto = req.params.texto;
+    const caracter = req.params.caracter;
+    const x = FreqCaracter(texto, caracter);
+
+    resp.send({
+        FreqCaracter: x
+    });
+})
+
+server.post('/tabuada', (req,resp) =>{
+    const numero = req.body;
+    const x = tabuada(numero);
+
+    resp.send({
+        tabuada:x
+    });
 })
 
 export default server;
---
+++ src/Services.js
@@ -0,0 +1,74 @@
+
+
+
+export function somar(a,b){
+    return  a +b;
+}
+
+export function dobro(a){
+    return  a*2;
+}
+
+export function media(a,b,c){
+    return(a+b+c)/3;
+}
+
+export function temp(a){     
+    let x = a >= 37;   
+    return x;
+}
+
+export function tabuada(a){
+
+}
+
+export function corsimcornao(cor){
+    let a = cor == 'amarelo' || cor == 'azul' || cor == 'vermelho';
+    return a;
+}
+
+export function ingresso(dia, inteira, meia, nacionalidade){
+    let a = inteira * 28.50;
+    let b = meia * 14.25;
+
+    if( nacionalidade == 'Brasileira'){
+        a = inteira * 5.00;
+        b = meia * 5.00;
+
+        return a + b;
+    }
+    else if( dia == 'quarta'){
+        a = inteira * 14.25;
+        b = meia * 14.25;
+
+        return a + b;
+    }
+
+    return a + b;
+}
+
+export function FreqCaracter(texto, caracter){
+    let a = 0;
+
+    for(let cont = 0; cont < texto.length; cont++){
+        if( texto[cont].toUpperCase() = caracter.toUpperCase())
+            a++;
+    }
+    return a;
+}
+
+export function tabuada(numero){
+    let a =[0,0,0,0,0,0,0,0,0,0,0];
+    let soma = 0;
+    for(let i = 0; i <= 10; i++) {
+        let s = numero * soma;
+        a[i] = s;
+        soma++;
+    }
+    return a;
+}
+
+
+
+
+
